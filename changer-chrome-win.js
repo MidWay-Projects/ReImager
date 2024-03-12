f3180e052b827bc1f31990bcd1cb40c132998e06
Scorbunny <100584937+Scorbunny10@users.noreply.github.com>
2024-03-12 21:59:54 +0100

    Update changer-chrome-win.js
    Signed-off-by: Scorbunny <100584937+Scorbunny10@users.noreply.github.com>

--- changer-chrome-win.js
+++ changer-chrome-win.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Reimager | Changer | Chrome | Win |
-// @icon https://reimager.midway.ovh/reimagge.png
+// @icon https://reimager.midway.ovh/reimager.png
 // @author Scorbunny
 // @namespace   https://github.com/MidWay-Projects/ReImager/tree/main
 // @version     1.3
